132a8ae31ee739bf3fc3279dd4875bab21291246
ParadoxRobotics <45688885+ParadoxRobotics@users.noreply.github.com>
2022-07-26 15:09:27 +0200

    Update PatchDCN.hpp
    first commit

--- include/PatchDCN.hpp
+++ include/PatchDCN.hpp
@@ -1,1 +1,54 @@
 
+// Robot Navigation stack -> Patch Divisive contrast Normalization pre-processing
+// Author : Munch Quentin, 2022.
+
+#pragma once
+// general lib
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+#include <vector>
+// OpenCV specific lib
+#include <opencv2/opencv.hpp> // Include OpenCV API
+#include <opencv2/imgproc.hpp> // Include basic image processing OpenCV API
+#include <opencv2/core/utility.hpp> // Include basic CV utilities
+#include <opencv2/core/types_c.h> // Include special CV structure
+#include <opencv2/highgui.hpp> // Include basic CV GUI
+
+// Patch DCN-like function
+void PatchDCN(cv::Mat Img, cv::Mat& out, std::vector<int> PatchSize, std::vector<int> Padding, float eps){
+  // Check if the image is valid
+  if (!Img.data || Img.empty()){
+    std::wcout << "Image Error" << std::endl;
+    return EXIT_FAILURE;
+  }
+  // get full image ROI
+  cv::Rect fullROI(0, 0, Img.cols, Img.cols);
+  // Input image Gaussian smoothing
+  cv::Mat smoothImg;
+  GaussianBlur(Img, smoothImg, Size(PatchSize[0], PatchSize[1]), 0, 0);
+  // Get the image size
+  int H = Img.rows;
+  int W = Img.cols;
+  // Compute the number of patch given their size
+  int row = (H / PatchSize[0]) + (H % PatchSize[0] ? 1 : 0);
+  int col = (W / PatchSize[1]) + (W % PatchSize[1] ? 1 : 0);
+  // perform local normalization P = P-Mean(P)/std(P)
+  for (int y = 0; y < row; y++){
+    for (int x = 0; x < col; x++){
+      // Get the patch
+      cv::Rect patchROI(x*PatchSize[1]-Padding[1], y*PatchSize[0]-Padding[0], PatchSize[1]+2*Padding[1], PatchSize[0]+2*Padding[0]);
+      patchROI = patchROI & fullROI;
+      cv::Mat patch = Img(patchROI);
+      cv::Mat smoothPatch = smoothImg(patchROI);
+      // Normalize it
+      cv::Scalar mean, stddev;
+      cv::meanStdDev(smoothPatch, mean, stddev);
+      cv::Mat tmpPatch = (patch - mean[0]) / (1 + eps*(stddev[0]+1e-5));
+      tmpPatch.copyTo(out);
+    }
+  }
+}
